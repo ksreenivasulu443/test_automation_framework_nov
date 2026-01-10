a5af53ae263c92337b41f6f389703d153972ac07
admin <kattubadi426@gmail.com>
2026-01-10 12:04:16 +0530

    New commit

---
+++ runner.py
@@ -0,0 +1,25 @@
+from utility.read_library import read_db, read_files
+import pandas as pd
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+testcases = pd.read_excel("/Users/admin/PycharmProjects/test_automation_framework_nov/configs/test_cases.xlsx")
+
+print("test cases:")
+print(testcases)
+testcases = testcases.query("execution_ind=='Y' ")
+
+print("test cases after selection exe = 'Y' :")
+print(testcases)
+
+for ind, row in testcases.iterrows():
+    print("==" * 100)
+    print("ind", ind)
+    print("=="*100)
+    print("row", row)
+    print("==" * 100)
+    print("type(row)", type(row))
+    print("row['source_type']",row['source_type'])
+    print("row['source_path']", row['source_path'])
+
+    source = read_files(path=row['source_path'],file_type=row['source_type'], header=0, sep=',')
+    print(source)
--- utility/read_library.py
+++ utility/read_library.py
@@ -1,3 +1,44 @@
 #Two function
 # 1. read files data
 # 2. read db data
+
+
+import pandas as pd
+import snowflake.connector as snow
+from pandasql import sqldf
+
+
+def read_files(path, file_type, header=0, sep='|'):
+    file_type = file_type.lower()
+    if file_type == 'csv':
+        df = pd.read_csv(path, header=header, sep=sep)
+    elif file_type == 'excel':
+        df = pd.read_excel(path)
+    elif file_type == 'parquet':
+        df = pd.read_parquet(path)
+    elif file_type == 'txt':
+        df = pd.read_csv(path)
+    elif file_type == 'json':
+        df = pd.read_json(path)
+    else:
+        print("Entered file format is not covered as part of current implementation. Please try with csv, excel and parquet")
+        df = pd.DataFrame()
+
+    return df
+
+def read_db(database_type,query, creds_file, env='qa'):
+    creds = pd.read_excel(creds_file)
+    print("creds", creds)
+    print("creds", creds.query(f"database_type == '{env}' "))
+    creds = creds.query(f"database_type == '{env}' ")
+
+    conn = snow.connect(
+        user=creds.loc[0, 'username'],
+        password=creds.loc[0, 'password'],
+        account=creds.loc[0, 'account'],
+        warehouse=creds.loc[0, 'warehouse']
+    )
+
+    print("conn", conn)
+    df = pd.read_sql(sql=query, con=conn)
+    return df
